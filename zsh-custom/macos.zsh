e1424477a87927539655452aa2d2bf750b0661fe
M. Tarık Yurt <mtyurt@gmail.com>
2021-01-28 10:55:45 +0300

    Use lsd for ls

--- zsh-custom/macos.zsh
+++ zsh-custom/macos.zsh
@@ -19,8 +19,8 @@ sleepafter() {
 #unset LSCOLORS
 #export CLICOLOR=1
 #export CLICOLOR_FORCE=1
-# alias ls='ls -GpF'   # Mac OSX specific
-alias ll="/usr/local/bin/ls_extended -hsla" # Mac OSX specific
+alias ls='lsd'
+alias ll="ls -l" # Mac OSX specific
 
 # other
 alias preview='open -a Preview'
