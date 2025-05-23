0c8cbfd3ef365741ff7590035eb8834f5e908b73
M. Tarık Yurt <mtyurt@gmail.com>
2025-05-23 13:17:49 +0300

    nvim: add diffview

--- nvim/init.lua
+++ nvim/init.lua
@@ -69,6 +69,7 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
+  'sindrets/diffview.nvim',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
